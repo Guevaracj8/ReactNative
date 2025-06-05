f3de8673d392e1c8aee5c9f32637173573a901b7
Guevaracj8 <guevaracj8@gmail.com>
2025-06-05 09:15:35 -0400

    Evaluacion I

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -40,6 +40,20 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="homeTab"
+        options={{
+          title: 'Home2',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settingsTab"
+        options={{
+          title: 'Configuration',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="gearshape.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
---
+++ app/(tabs)/settingsTab.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function SettingsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Pantalla de Configuración</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+  text: {
+    color: 'skyblue', // Celeste
+    fontSize: 18,
+  },
+});
